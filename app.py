03d5d31cb3d9143acc4c87e6d3f14b28bf8b5761
EC2 Default User <ec2-user@ip-172-31-40-74.ec2.internal>
2021-02-07 16:21:56 +0000

    drastically change code to incorporate showing random information for each song, also moved code into different methods for a cleaner look and better readablity

--- app.py
+++ app.py
@@ -5,55 +5,59 @@ from flask import Flask, render_template
 import random
 
 # SPOTIFY API
-load_dotenv(find_dotenv())
-
-CLIENT_ID = os.getenv('client-id')
-CLIENT_SECRET = os.getenv('client-secret')
-
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-
-# POST
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-})
-
-# convert the response to JSON
-auth_response_data = auth_response.json()
-
-# save the access token
-access_token = auth_response_data['access_token']
-#print(access_token)
-
-data = {'Authorization': 'Bearer {token}'.format(token=access_token)}
-
-ids = [
-'4O15NlyKLIASxsJ0PrXPfz',
-'0xOeVMOz2fVg5BJY3N6akT',
-'4Gso3d4CscCijv0lmajZWs',
-'5K4W6rqBFWDnAN6FQUkS6x',
-'4LLpKhyESsyAXpc4laK94U',
-'1Bl6wpkWCQ4KVgnASpvzzA',
-'5he5w2lnU9x7JFhnwcekXX',
-'64KEffDW9EtZ1y2vBYgq8T',
-'7dGJo4pcD2V6oG8kP0tJRR',
-'246dkjvS1zLTtiykXe5h60',
-'6eUKZXaKkcviH0Ku9w2n3V',
-'6qqNVTkY8uBg9cP3Jd7DAH'
-]
-
-
-# FLASK
-
-app = Flask(__name__)
-
-@app.route('/')
-def spotify():
-    print("Updated printline")
+def GET_AUTHORIZATION():
+    load_dotenv(find_dotenv())
     
+    CLIENT_ID = os.getenv('client-id')
+    CLIENT_SECRET = os.getenv('client-secret')
+    
+    AUTH_URL = 'https://accounts.spotify.com/api/token'
+    
+    # POST
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    })
+    
+    # convert the response to JSON
+    auth_response_data = auth_response.json()
+    
+    # save the access token
+    access_token = auth_response_data['access_token']
+    #print(access_token)
+    
+    data = {'Authorization': 'Bearer {token}'.format(token=access_token)}
+    
+    return data
+
+
+
+def get_song_info(data):
+    ids = [
+    '4O15NlyKLIASxsJ0PrXPfz',
+    '0xOeVMOz2fVg5BJY3N6akT',
+    '4Gso3d4CscCijv0lmajZWs',
+    '5K4W6rqBFWDnAN6FQUkS6x',
+    '4LLpKhyESsyAXpc4laK94U',
+    '1Bl6wpkWCQ4KVgnASpvzzA',
+    '5he5w2lnU9x7JFhnwcekXX',
+    '64KEffDW9EtZ1y2vBYgq8T',
+    '7dGJo4pcD2V6oG8kP0tJRR',
+    '246dkjvS1zLTtiykXe5h60',
+    '6eUKZXaKkcviH0Ku9w2n3V',
+    '6qqNVTkY8uBg9cP3Jd7DAH'
+    ]
+    
+    # decide which information to send
+    rand_info = []
+    for i in range(4):
+        rand_info.append(random.randint(0,1) )
     song_audio = None
-    while song_audio == None:
+    
+    
+    # do while, conditions below
+    while True:
         i = random.randint(0,len(ids) - 1)
         add = requests.get(
             'https://api.spotify.com/v1/artists/{}/top-tracks'.format(ids[i]), 
@@ -63,23 +67,42 @@ def spotify():
         add = add.json()
         
         
-        track_num = len(add['tracks'])
+       # track_num = len(add['tracks'])
     
-        rand = random.randint(0, track_num-1)
-       
-        song_name = add['tracks'][rand]['name'] 
-        song_artist = add['tracks'][rand]['artists'][0]['name']
-        song_image = add['tracks'][rand]['album']['images'][0]['url']
-        song_audio = add['tracks'][rand]['preview_url'] 
+        rand = random.randint(0, 9) #track_num-1)
+        # sometimes getting errors that 'tracks' is not a good keywork sometimes it happens and sometimes it doesn't
+        try:
+            song_name = add['tracks'][rand]['name'] 
+            song_artist = add['tracks'][rand]['artists'][0]['name']
+            song_image = add['tracks'][rand]['album']['images'][0]['url']
+            song_audio = add['tracks'][rand]['preview_url'] 
+        except:
+            continue
+        
+        # do while conditions: if we send audio, keep finding different songs until audio is grabbed
+        if rand_info[3] == 0:
+            break
+        if song_audio != None and rand_info[3] == 1:
+            break
+        
+    return [song_name, song_artist, song_image, song_audio, rand_info]
 
+# FLASK
+
+app = Flask(__name__)
+
+@app.route('/')
+def spotify():
+    data = GET_AUTHORIZATION()
+    info = get_song_info(data)
     
     return render_template(
         "index.html", 
-        song_name = song_name,
-        song_artist = song_artist,
-        song_image = song_image,
-        song_audio = song_audio,
-        rand_info = random.randint(1,4)
+        song_name = info[0],
+        song_artist = info[1],
+        song_image = info[2],
+        song_audio = info[3],
+        rand_info = info[4]
     )
 
 
